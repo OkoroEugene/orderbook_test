8b88556397800868256073db966cdeb7b9f3bf2a
Obinna Okoro <obinna.okoroeugene@gmail.com>
2021-07-08 10:06:08 +0100

    setup actions for orderbook bids/asks with redux

--- src/containers/OrderBook.js
+++ src/containers/OrderBook.js
@@ -1,32 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import {updateOrderBids} from '../actions/OrderBooks/UpdateBids';
+import {updateOrderAsks} from '../actions/OrderBooks/UpdateAsks';
+import {clearOrders} from '../actions/OrderBooks/ClearOrders';
+import { useCallback } from 'react';
 
 function OrderBook(props) {
-    useEffect(() => {
-        const subscribe_msg = JSON.stringify({
-            event: 'subscribe',
-            channel: 'book',
-            symbol: 'tBTCUSD'
-        })
+    const wss = new WebSocket('wss://api-pub.bitfinex.com/ws/2');
+    const {
+        updateOrderBids, 
+        updateOrderAsks,
+        clearOrders,
+        orderbook:{
+            order_books: {bids,asks}
+        }
+    } = props;
 
-        const unsubscribe_msg = JSON.stringify({
-            event: 'unsubscribe',
-            channel: 'book',
-        })
+    const subscribe_msg = JSON.stringify({
+        event: 'subscribe',
+        channel: 'book',
+        symbol: 'tBTCUSD'
+    })
 
-        const wss = new WebSocket('wss://api-pub.bitfinex.com/ws/2');
+    const unsubscribe_msg = JSON.stringify({
+        event: 'unsubscribe',
+        channel: 'book',
+    })
+
+    const connectAll = () => {
         wss.onmessage = (msg) => onMessageReceived(msg);
         wss.onopen = () => {
             wss.send(subscribe_msg);
         }
+    }
 
-        return (() => {
-            if (wss.OPEN && !wss.CONNECTING) {
-                wss.send(unsubscribe_msg);
-                wss.close();
-            }
-        })
-    })
+    const disconnectAll = () => {
+        clearOrders();
+        wss.send(unsubscribe_msg);
+        wss.close();
+    }
 
     const onMessageReceived = (msg) => {
         const payload = JSON.parse(msg.data);
@@ -38,15 +52,38 @@ function OrderBook(props) {
                 count: snapshot[1],
                 amount: parseFloat(snapshot[2]).toFixed(2),
             }
-            // console.log(payload_data);
+            
+            if (typeof payload_data.count === 'number') {
+                if (payload_data.amount > '0.00') {
+                    updateOrderBids(payload_data);
+                }
+    
+                else {
+                    updateOrderAsks(payload_data);
+                }
+            }
         }
     }
 
     return (
         <View>
             <Text>Hello there!</Text>
+
+            {/* {bids.map((e, i) => <Text key={i}>{e.price}</Text>)} */}
         </View>
     );
 }
 
-export default OrderBook;
+function mapStateToProps(state) {
+    return {
+      orderbook: state.orderbook,
+    }
+  }
+  
+  const mapDispatchToProps = (dispatch) => bindActionCreators({
+      updateOrderBids,
+      updateOrderAsks,
+      clearOrders
+  }, dispatch);
+  
+  export default connect(mapStateToProps, mapDispatchToProps)(OrderBook);
--- src/reducers/orderbook.js
+++ src/reducers/orderbook.js
@@ -9,6 +9,62 @@ export const initialState = {
 
 export default function reducer(state = initialState, action) {
     switch (action.type) {
+        case types.UPDATE_ORDER_BOOK_BIDS: {
+            let new_bids = [...state.order_books.bids, action.payload];
+            const { price, count, amount } = action.payload;
+            const index = new_bids.findIndex(c => c.price === price);
+
+            //check for existing price and update bid
+            if (state.order_books.bids.some(c => c.price === price)) {
+                state.order_books.bids[index].count = count;
+                state.order_books.bids[index].amount = amount;
+                new_bids = state.order_books.bids;
+            }
+
+            //if amount == 1 remove from bids
+            if (count === 0 && amount === '1.00') {
+                new_bids.splice(index, 1);
+            }
+
+            //else if amount > 0 add to bids
+            new_bids = new_bids.sort((a, b) => b.price - a.price);
+            return {
+                ...state,
+                order_books: {bids: new_bids, asks: state.order_books.asks},
+            }
+        }
+
+        case types.UPDATE_ORDER_BOOK_ASKS: {
+            let new_asks = [...state.order_books.asks, action.payload];
+            const { price, count, amount } = action.payload;
+            const index = new_asks.findIndex(c => c.price === price);
+
+            //check for existing price and update ask
+            if (state.order_books.asks.some(c => c.price === price)) {
+                state.order_books.asks[index].count = count;
+                state.order_books.asks[index].amount = amount;
+                new_asks = state.order_books.asks;
+            }
+
+            //if amount == -1 remove from asks
+            if (count === 0 && amount === '-1.00') {
+                new_asks.splice(index, 1);
+            }
+
+            //else if amount > 0 add to asks
+            new_asks = new_asks.sort((a, b) => b.price - a.price);
+            return {
+                ...state,
+                order_books: {asks: new_asks, bids: state.order_books.bids},
+            }
+        }
+
+        case types.CLEAR_ORDERS:
+            return {
+                ...state,
+                order_books: {bids:[], asks:[]}
+            }
+
         default:
             return state;
     }
